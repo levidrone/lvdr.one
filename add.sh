bbcab48bbba8d52492dd320e071240626f7bfea7
Shamil Yakupov <s.yakupov@noviga.com>
2018-12-02 20:57:04 +0300

    Make adding script commit generated page to git

--- add.sh
+++ add.sh
@@ -10,7 +10,8 @@ echo -n "Short URL: $SHORT_URL/"
 read SOURCE_PATH
 echo -n "Redirect to URL: $LONG_URL/"
 read TARGET_PATH
-echo -e "\n$SHORT_URL/$SOURCE_PATH -> $LONG_URL/$TARGET_PATH"
+MESSAGE="$SHORT_URL/$SOURCE_PATH -> $LONG_URL/$TARGET_PATH"
+echo -e "\n$MESSAGE"
 echo -n "All right? (y/N): "
 read ANSWER
 
@@ -20,6 +21,11 @@ if [ "$ANSWER" == "y" ] || [ "$ANSWER" == "Y" ]; then
   cd $SOURCE_PATH
   echo "Generate page: '$SOURCE_PATH/index.html'"
   echo "<html><head><meta http-equiv=\"refresh\" content\"0; url=$LONG_URL/$TARGET_PATH\" /></head><body></body></html>" > index.html
-  echo -e "\nDone! Commit, push and enjoy!"
+  echo "Add to git: '$SOURCE_PATH/index.html'"
+  git add index.html
+  GIT_MESSAGE="Add redirect: $MESSAGE"
+  echo "Commit with message: '$GIT_MESSAGE'"
+  git commit -m "$GIT_MESSAGE"
+  echo -e "\nDone! Push and enjoy!"
 fi
 
